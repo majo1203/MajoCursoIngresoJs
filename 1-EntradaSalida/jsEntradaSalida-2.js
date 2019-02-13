82b77e1a81b7fa1e928e643880727c994ea262ba
MariaJose <maria-jose1203@hotmail.com>
2019-02-13 00:02:35 -0300

    Commit inicial

---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,12 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var numero;
+	var respuesta;
+
+	respuesta= prompt("ingrese su numero");
+
+	alert("Su respuesta es: " + respuesta);
+}
+
