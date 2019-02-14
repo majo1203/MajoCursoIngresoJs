e0180bde274bdb14915641e0dd96292ab871a399
MariaJose <maria-jose1203@hotmail.com>
2019-02-14 01:02:03 -0300

    ejercicio 9 malo

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var Resultado;
+	var aumento;
 
+	sueldo = document.getElementById("sueldo").value;
+	Resultado = document.getElementById("Resultado");
+	aumento = (sueldo) 10% (Resultado);
+
+	alert("Su resultado es " + Resultado);
 	
 }
